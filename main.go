eefe05c31f7f71413c86023df011056986814175
faizando <faizanj@nandos.co.uk>
2021-02-08 21:17:59 +0000

    wip add *rover functions

--- main.go
+++ main.go
@@ -117,3 +117,105 @@ func getRover(roverInput string) rover {
 
 	return r
 }
+
+func processRoversAndPrintResult(p plateau, rl []rover) {
+
+	// move rover and when cannot move further and wont collide print it
+	// so not looping twice, more elegant solution
+
+	for i, r := range rl {
+
+		// rover within plateau and >= 0,0
+		if r.x >= 0 && r.x <= p.x && r.y >= 0 && r.y <= p.y {
+
+			// compare to cr (control rover)
+			for ci, cr := range rl {
+				// index not same so that do not check collision with itself
+				// TODO should check will not collide with the most up to date rover x,r not the control because here control is original copy of the input!
+				if ci != i && roverWillNotCollide(r, cr) {
+					processCommand(r)
+					r.move()
+					r.print()
+				}
+			}
+
+		}
+	}
+
+}
+
+func processCommand(r rover, c string) (rover, error) {
+	validCommands := "LRM"
+
+	rt := r
+
+	if strings.Contains(validCommands, c) {
+		switch c {
+		case "L":
+		case "R":
+			rt.spin(c)
+		case "M":
+			// todo check collision here before moving?
+			rt.move()
+		}
+	} else {
+		fmt.Println("Error, command ", c, " to rover is invalid")
+		os.Exit(1)
+		// return rt, nil
+	}
+
+	return rt, nil
+
+}
+
+func roverWillNotCollide(r rover, cr rover) bool {
+	r2 := r
+	r2.move()
+	if r2.x != cr.x && r2.y != cr.y {
+		return true
+	}
+	return false
+}
+
+func (r *rover) print() {
+	fmt.Println(r.x, r.y, r.dir)
+}
+
+func (r *rover) spin(d string) {
+	if d == "R" {
+		switch r.dir {
+		case "E":
+			r.dir = "S"
+		case "S":
+			r.dir = "W"
+		case "W":
+			r.dir = "N"
+		case "N":
+			r.dir = "E"
+		}
+	} else if d == "L" {
+		switch r.dir {
+		case "S":
+			r.dir = "E"
+		case "W":
+			r.dir = "S"
+		case "N":
+			r.dir = "W"
+		case "E":
+			r.dir = "N"
+		}
+	}
+}
+
+func (r *rover) move() {
+	switch r.dir {
+	case "E":
+		r.x = r.x + 1
+	case "W":
+		r.x = r.x - 1
+	case "N":
+		r.y = r.y + 1
+	case "S":
+		r.y = r.y - 1
+	}
+}
