726819e41e2528048355ba0b84badb3a1a7c7e3a
faizando <faizanj@nandos.co.uk>
2021-02-12 12:33:58 +0000

    move rover related fun into tover.go

--- main.go
+++ main.go
@@ -52,58 +52,6 @@ func getPlateauCoordinates(line string) plateau {
 	return p
 }
 
-func getListOfRovers(input []string) []rover {
-	rl := []rover{}
-
-	// flatten 2 line input per rover into one line
-	for i := 1; i < len(input); i += 2 {
-
-		ri := input[i-1] + " " + input[i]
-		rl = append(rl, getRover(ri))
-	}
-
-	return rl
-}
-
-func getRover(roverInput string) rover {
-	validDirs := "NEWS"
-	char := strings.Fields(roverInput)
-
-	// Assumption: rover always given coordinates x,y, direction, and command(s)
-	if len(char) != 4 {
-		fmt.Println("Error, rover input values provided ", char, "are invalid")
-		os.Exit(1)
-	}
-
-	r := rover{}
-
-	if cx, err := strconv.Atoi(char[0]); err == nil {
-		if cy, err := strconv.Atoi(char[1]); err == nil {
-
-			// Assumption will allow rovers to land outside 0,plateau, life
-			r.x = cx
-			r.y = cy
-			r.com = char[3]
-
-			if strings.Contains(validDirs, char[2]) {
-				r.dir = char[2]
-			} else {
-				fmt.Println("Error, rover input direction '", char[2], "' is invalid")
-				os.Exit(1)
-			}
-
-		} else {
-			fmt.Println("Error, could not read rover input values", err)
-			os.Exit(1)
-		}
-	} else {
-		fmt.Println("Error, could not read rover input values", err)
-		os.Exit(1)
-	}
-
-	return r
-}
-
 func processRoversAndPrintResult(p plateau, roverlist []rover) {
 
 	rl := roverlist
@@ -153,13 +101,4 @@ func processRoversAndPrintResult(p plateau, roverlist []rover) {
 		rl[i].print()
 	}
 
-}
-
-func roverWillCollide(r rover, cr rover) bool {
-	r2 := r
-	r2.move()
-	if r2.x == cr.x && r2.y == cr.y {
-		return true
-	}
-	return false
 }
