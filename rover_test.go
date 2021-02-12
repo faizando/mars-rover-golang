6920d36f79451f89ddb117225e280d5956905426
faizando <faizanj@nandos.co.uk>
2021-02-12 16:54:54 +0000

    Add Tests for rover.go

--- rover_test.go
+++ rover_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -61,4 +63,67 @@ func TestRoverMove(t *testing.T) {
 		t.Errorf("Expected rover to be {x: 1, y: 1}, but got rover: %v", r)
 	}
 
+}
+
+func TestRoverWillCollide(t *testing.T) {
+	r1 := rover{x: 1, y: 1, dir: "N", com: "LLM"}
+	r2 := rover{x: 1, y: 2, dir: "N", com: "LLM"}
+	r3 := rover{x: 3, y: 3, dir: "N", com: "LLM"}
+
+	if !roverWillCollide(r1, r2) {
+		t.Errorf("Expected rover to collide, got false")
+	}
+
+	if roverWillCollide(r1, r3) {
+		t.Errorf("Expected rover to collide, got false")
+	}
+}
+
+func TestGetRover(t *testing.T) {
+
+	type testCase struct {
+		input       string
+		expectedRov rover
+		expectedErr error
+	}
+
+	tr := rover{}
+
+	tests := []testCase{
+		{input: "1 1 N L", expectedRov: rover{x: 1, y: 1, dir: "N", com: "L"}, expectedErr: nil},
+		{input: "1 1 N", expectedRov: tr, expectedErr: fmt.Errorf("Error, rover input values provided [1 1 N] are invalid")},
+		{input: "1 1 blah L", expectedRov: tr, expectedErr: fmt.Errorf("Error, rover input direction 'blah' is invalid")},
+		{input: "a 1 N L", expectedRov: tr, expectedErr: fmt.Errorf("Error, input rover x coordinate value of 'a' is invalid")},
+		{input: "1 b N L", expectedRov: tr, expectedErr: fmt.Errorf("Error, input rover y coordinate value of 'b' is invalid")},
+	}
+
+	for _, tc := range tests {
+		gotRov, gotErr := getRover(tc.input)
+		if gotErr == nil && !reflect.DeepEqual(gotRov, tc.expectedRov) {
+			t.Errorf("Expected: %v, \n Got: %v", tc.expectedRov, gotRov)
+		}
+
+		if !reflect.DeepEqual(gotErr, tc.expectedErr) {
+			t.Errorf("Expected: %v, \n Got: %v", tc.expectedErr, gotErr)
+		}
+	}
+}
+
+func TestGetListOfRovers(t *testing.T) {
+
+	validInput := []string{"1 2 N", "L", "3 3 E", "M"}
+	expected := []rover{rover{x: 1, y: 2, dir: "N", com: "L"},
+		rover{x: 3, y: 3, dir: "E", com: "M"}}
+
+	got, _ := getListOfRovers(validInput)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("Expected: %v, \n Got: %v", expected, got)
+	}
+
+	invalidInput := []string{"blah 2 N", "L", "3 3 E", "M"}
+	expectedErr := fmt.Errorf("Error, input rover x coordinate value of 'blah' is invalid")
+	_, err := getListOfRovers(invalidInput)
+	if !reflect.DeepEqual(err, expectedErr) {
+		t.Errorf("Expected: %v, \n Got: %v", expectedErr, err)
+	}
 }
